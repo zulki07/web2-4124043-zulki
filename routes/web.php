904cd740f1aa4e07d775cdd01bdb7b0c4f62f412
zulki <zulkitahit@gmail.com>
2026-03-05 13:56:55 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,5 @@
+Route::get('/perkenalan', function () {
+    return '<h1>Halo! Nama saya zulki</h1>
+            <p>NIM: 4124043 | Prodi: Sistem Informasi</p>
+            <p>Saya siap belajar Laravel! 🚀</p>';
+});
